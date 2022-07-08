e3c87f978c75faced00b8032bac23505ff928abd
Tuğrul Özpehlivan <94709687+Tgrlzphlvn@users.noreply.github.com>
2022-07-08 03:36:01 +0300

    Add files via upload

---
+++ Feature/swiftUIReqresApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIReqresApiApp.swift
+//  swiftUIReqresApi
+//
+//  Created by Tuğrul Özpehlivan on 8.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIReqresApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ReqresView()
+        }
+    }
+}
